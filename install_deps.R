bc35d06929e52ad4afa058630490c19f01755d89
Dominic Bennett <dominic.john.bennett@gmail.com>
2019-10-21 11:42:13 +0200

    manual install

---
+++ install_deps.R
@@ -0,0 +1,7 @@
+source("https://raw.githubusercontent.com/r-lib/remotes/master/install-github.R")$value("r-lib/remotes")
+username <- 'AntonelliLab'
+repos <- c('outsider.base', 'outsider', 'outsider.devtools')
+for (repo in repos) {
+  cat('... Installing [', repo, ']\n')
+  remotes::install_github(repo = paste0(username, '/', repo))
+}
